3af62cf3057aeda358a577684b81f71ae70630dd
Daniel <du.ubanan@gmail.com>
2018-11-17 17:35:55 +0800

    hello world

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,27 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import {UserService} from '../services/user.service';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit, NgModule } from "@angular/core";
+import { UserService } from "../services/user.service";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  selector: "app-user",
+  templateUrl: "./user.component.html",
+  styleUrls: ["./user.component.css"]
 })
 export class UserComponent implements OnInit {
   users: Array<any>;
-  
-  constructor(private userService:UserService) { }
+
+  constructor(private userService: UserService) {}
 
   ngOnInit() {
-    this.userService.getAll().subscribe(data =>{
+    this.userService.getAll().subscribe(data => {
       this.users = data;
     });
   }
 
-  verify(): void{
-    this.userService.verifyUser().subscribe(data =>{
-      console.log(data);
-    }, error => console.error(error));
+  verify(): void {
+    this.userService.verifyUser().subscribe(
+      data => {
+        console.log(data);
+      },
+      error => console.error(error)
+    );
   }
-
 }
