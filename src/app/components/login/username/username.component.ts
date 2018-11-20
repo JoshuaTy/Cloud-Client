f745ee7acad7dccec2e4766227e5e3f0faf7e947
Daniel <du.ubanan@gmail.com>
2018-11-20 22:32:53 +0800

    Added Username and Password Components

--- src/app/components/login/username/username.component.ts
+++ src/app/components/login/username/username.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-username',
-  templateUrl: './username.component.html',
-  styleUrls: ['./username.component.scss']
+  selector: "app-username",
+  templateUrl: "./username.component.html",
+  styleUrls: ["./username.component.scss"]
 })
 export class UsernameComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
