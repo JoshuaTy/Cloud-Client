0c8d1c58fbb251862c73ccb1882b3c986edbfb93
Joshua Ty <joshuaty98@gmail.com>
2018-11-20 22:26:20 +0800

    Updated login component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit, ChangeDetectionStrategy } from "@angular/core";
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+import { AuthenticationService } from "src/app/_services/authentication.service";
+import { AlertService } from "src/app/_services/alert.service";
 
 @Component({
   selector: "app-login",
@@ -7,7 +12,50 @@ import { Component, OnInit, ChangeDetectionStrategy } from "@angular/core";
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
+  loginForm: FormGroup;
+  loading = false;
+  submitted = false;
+  returnUrl: string;
+  constructor(
+            private formBuilder: FormBuilder,
+            private route: ActivatedRoute,
+            private router: Router,
+            private authenticationService: AuthenticationService,
+            private alertService: AlertService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+
+    // reset login status
+    this.authenticationService.logout();
+
+    // get return url from route parameters or default to '/'
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+  }
+   // convenience getter for easy access to form fields
+   get f() { return this.loginForm.controls; }
+ 
+   onSubmit() {
+       this.submitted = true;
+
+       // stop here if form is invalid
+       if (this.loginForm.invalid) {
+           return;
+       }
+
+       this.loading = true;
+       this.authenticationService.login(this.loginForm.value)
+           .pipe(first())
+           .subscribe(
+               data => {
+                   this.router.navigate([this.returnUrl]);
+               },
+               error => {
+                   this.alertService.error(error);
+                   this.loading = false;
+               });
+   }
 }
