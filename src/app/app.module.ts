fdd16271d43c09f004a1b0912b0c24072a23ae45
Daniel <du.ubanan@gmail.com>
2018-11-22 00:48:24 +0800

    Fixedinput form bug and cleaned up login component code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,23 +8,21 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NbThemeModule, NbLayoutModule } from "@nebular/theme";
 import { NebularModule } from "./modules/nebular/nebular.module";
 import { UserService } from "./_services/user.service";
-import { HttpClientModule,HTTP_INTERCEPTORS } from "@angular/common/http";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { AlertComponent } from "./_directives/alert.component";
 import { AlertService } from "./_services/alert.service";
 import { AuthenticationService } from "./_services/authentication.service";
 import { AuthGuard } from "./_guards/auth.guard";
+import { JwtInterceptor } from "./_helpers/jwt.interceptor";
+import { ErrorInterceptor } from "./_helpers/error.interceptor";
 
 import { EntryComponent } from "./components/entry/entry.component";
 import { LoginComponent } from "./components/login/login.component";
-import { UsernameComponent } from "./components/login/username/username.component";
-import { PasswordComponent } from "./components/login/password/password.component";
 import { RegisterComponent } from "./components/register/register.component";
 import { AdminComponent } from "./components/admin/admin.component";
 import { DiseaseComponent } from "./components/admin/disease/disease.component";
 import { MedicineComponent } from "./components/admin/medicine/medicine.component";
 import { NotFoundComponent } from "./components/not-found/not-found.component";
-import { JwtInterceptor } from "./_helpers/jwt.interceptor";
-import { ErrorInterceptor } from "./_helpers/error.interceptor";
 
 @NgModule({
   declarations: [
@@ -34,8 +32,6 @@ import { ErrorInterceptor } from "./_helpers/error.interceptor";
     RegisterComponent,
     AdminComponent,
     AlertComponent,
-    UsernameComponent,
-    PasswordComponent,
     DiseaseComponent,
     MedicineComponent,
     NotFoundComponent
@@ -52,12 +48,12 @@ import { ErrorInterceptor } from "./_helpers/error.interceptor";
     HttpClientModule
   ],
   providers: [
-    AuthGuard, 
-    UserService, 
-    AlertService, 
+    AuthGuard,
+    UserService,
+    AlertService,
     AuthenticationService,
-    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi:true},
-    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }
   ],
   bootstrap: [AppComponent]
 })
